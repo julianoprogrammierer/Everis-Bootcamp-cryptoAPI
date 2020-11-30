0ec1ee6c468c45cc19b67ebcc3017baee32788e5
julianoprogrammierer <julianoprogrammierer@gmail.com>
2020-11-29 21:10:21 -0300

    new

--- page/script.js
+++ page/script.js
@@ -23,7 +23,7 @@ fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY='
        texto = texto + `
       
         <div class="media">
-            <img src="coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+            <img src="/page/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
             <div class="media-body">
             <h5 class="mt-2">${api.data[i].name}</h5>
             <p>${api.data[i].symbol}</p>
